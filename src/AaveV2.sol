9b13be17c7ffeed5324692f67d42e131c0087d45
Lukas <lukasstrassel@googlemail.com>
2022-07-12 10:27:43 +0200

    feat: v2 configuration inputs (#18)

--- src/AaveV2.sol
+++ src/AaveV2.sol
@@ -52,6 +52,46 @@ library DataTypes {
   }
 }
 
+library ConfiguratorInputTypes {
+  struct InitReserveInput {
+    address aTokenImpl;
+    address stableDebtTokenImpl;
+    address variableDebtTokenImpl;
+    uint8 underlyingAssetDecimals;
+    address interestRateStrategyAddress;
+    address underlyingAsset;
+    address treasury;
+    address incentivesController;
+    string underlyingAssetName;
+    string aTokenName;
+    string aTokenSymbol;
+    string variableDebtTokenName;
+    string variableDebtTokenSymbol;
+    string stableDebtTokenName;
+    string stableDebtTokenSymbol;
+    bytes params;
+  }
+
+  struct UpdateATokenInput {
+    address asset;
+    address treasury;
+    address incentivesController;
+    string name;
+    string symbol;
+    address implementation;
+    bytes params;
+  }
+
+  struct UpdateDebtTokenInput {
+    address asset;
+    address incentivesController;
+    string name;
+    string symbol;
+    address implementation;
+    bytes params;
+  }
+}
+
 interface ILendingPoolAddressesProvider {
   event MarketIdSet(string newMarketId);
   event LendingPoolUpdated(address indexed newAddress);
