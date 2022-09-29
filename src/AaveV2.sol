eafb2e79679496996960b43daa240407a4684988
Andrey <kyzia.ru@gmail.com>
2022-09-29 14:17:04 +0200

    fat: add getUserReserveData method to IAaveProtocolDataProvider (#31)
    * add getUserReserveData method to IAaveProtocolDataProvider
    
    * fix: update tests as polygon was updated
    
    Co-authored-by: Lukas <lukasstrassel@googlemail.com>

--- src/AaveV2.sol
+++ src/AaveV2.sol
@@ -928,4 +928,19 @@ interface IAaveProtocolDataProvider {
       address stableDebtTokenAddress,
       address variableDebtTokenAddress
     );
+
+  function getUserReserveData(address asset, address user)
+    external
+    view
+    returns (
+      uint256 currentATokenBalance,
+      uint256 currentStableDebt,
+      uint256 currentVariableDebt,
+      uint256 principalStableDebt,
+      uint256 scaledVariableDebt,
+      uint256 stableBorrowRate,
+      uint256 liquidityRate,
+      uint40 stableRateLastUpdated,
+      bool usageAsCollateralEnabled
+    );
 }
