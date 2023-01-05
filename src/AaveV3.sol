c518993fcab70f8766f3f3ba154d4ace05cb3770
Ernesto Boado <ebdmrr@gmail.com>
2023-01-05 17:51:32 +0100

    Added hasRole() to v3 IACLManager (#56)
    * Added hasRole() to v3 IACLManager
    
    * - Added getter for DEFAULT_ADMIN_ROLE on extended IACLManager.

--- src/AaveV3.sol
+++ src/AaveV3.sol
@@ -8,11 +8,11 @@ import {IPool} from 'aave-v3-core/contracts/interfaces/IPool.sol';
 import {IPoolConfigurator} from 'aave-v3-core/contracts/interfaces/IPoolConfigurator.sol';
 import {IPriceOracleGetter} from 'aave-v3-core/contracts/interfaces/IPriceOracleGetter.sol';
 import {IAaveOracle} from 'aave-v3-core/contracts/interfaces/IAaveOracle.sol';
-import {IACLManager} from 'aave-v3-core/contracts/interfaces/IACLManager.sol';
+import {IACLManager as BasicIACLManager} from 'aave-v3-core/contracts/interfaces/IACLManager.sol';
 import {IPoolDataProvider} from 'aave-v3-core/contracts/interfaces/IPoolDataProvider.sol';
-import {IDefaultInterestRateStrategy} from "aave-v3-core/contracts/interfaces/IDefaultInterestRateStrategy.sol";
-import {IReserveInterestRateStrategy} from "aave-v3-core/contracts/interfaces/IReserveInterestRateStrategy.sol";
-import {IDefaultInterestRateStrategy as IInterestRateStrategy} from "aave-v3-core/contracts/interfaces/IDefaultInterestRateStrategy.sol";
+import {IDefaultInterestRateStrategy} from 'aave-v3-core/contracts/interfaces/IDefaultInterestRateStrategy.sol';
+import {IReserveInterestRateStrategy} from 'aave-v3-core/contracts/interfaces/IReserveInterestRateStrategy.sol';
+import {IDefaultInterestRateStrategy as IInterestRateStrategy} from 'aave-v3-core/contracts/interfaces/IDefaultInterestRateStrategy.sol';
 import {IPoolDataProvider as IAaveProtocolDataProvider} from 'aave-v3-core/contracts/interfaces/IPoolDataProvider.sol';
 
 /**
@@ -71,4 +71,10 @@ interface ICollector {
    * @param admin The address of the new funds administrator
    */
   function setFundsAdmin(address admin) external;
+}
+
+interface IACLManager is BasicIACLManager {
+  function hasRole(bytes32 role, address account) external view returns (bool);
+
+  function DEFAULT_ADMIN_ROLE() external pure returns (bytes32);
 }
