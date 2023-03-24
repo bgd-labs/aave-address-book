b2d4bdefa44099a672e5b3eab8ef06998bb2a5e1
Lukas <lukasstrassel@googlemail.com>
2023-03-24 10:44:49 +0100

    feat: reexport aggregator intreface (#110)

--- src/AaveV2.sol
+++ src/AaveV2.sol
@@ -2,6 +2,8 @@
 pragma solidity >=0.6.0;
 pragma experimental ABIEncoderV2;
 
+import {AggregatorInterface} from 'aave-v3-core/contracts/dependencies/chainlink/AggregatorInterface.sol';
+
 library DataTypes {
   // refer to the whitepaper, section 1.1 basic concepts for a formal description of these properties.
   struct ReserveData {
