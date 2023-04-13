e86c022933f9f207a82a718c45dd16ea40d7885f
Lukas <lukasstrassel@googlemail.com>
2023-04-13 12:02:55 +0200

    feat: add streamable interface

--- src/AaveMisc.sol
+++ src/AaveMisc.sol
@@ -74,6 +74,72 @@ interface IAaveEcosystemReserveController {
   function cancelStream(address collector, uint256 streamId) external returns (bool);
 }
 
+interface IStreamable {
+  struct Stream {
+    uint256 deposit;
+    uint256 ratePerSecond;
+    uint256 remainingBalance;
+    uint256 startTime;
+    uint256 stopTime;
+    address recipient;
+    address sender;
+    address tokenAddress;
+    bool isEntity;
+  }
+
+  event CreateStream(
+    uint256 indexed streamId,
+    address indexed sender,
+    address indexed recipient,
+    uint256 deposit,
+    address tokenAddress,
+    uint256 startTime,
+    uint256 stopTime
+  );
+
+  event WithdrawFromStream(uint256 indexed streamId, address indexed recipient, uint256 amount);
+
+  event CancelStream(
+    uint256 indexed streamId,
+    address indexed sender,
+    address indexed recipient,
+    uint256 senderBalance,
+    uint256 recipientBalance
+  );
+
+  function balanceOf(uint256 streamId, address who) external view returns (uint256 balance);
+
+  function getStream(
+    uint256 streamId
+  )
+    external
+    view
+    returns (
+      address sender,
+      address recipient,
+      uint256 deposit,
+      address token,
+      uint256 startTime,
+      uint256 stopTime,
+      uint256 remainingBalance,
+      uint256 ratePerSecond
+    );
+
+  function createStream(
+    address recipient,
+    uint256 deposit,
+    address tokenAddress,
+    uint256 startTime,
+    uint256 stopTime
+  ) external returns (uint256 streamId);
+
+  function withdrawFromStream(uint256 streamId, uint256 funds) external returns (bool);
+
+  function cancelStream(uint256 streamId) external returns (bool);
+
+  function initialize(address fundsAdmin) external;
+}
+
 library AaveMisc {
   address internal constant ECOSYSTEM_RESERVE = 0x25F2226B597E8F9514B3F68F00f494cF4f286491;
 
