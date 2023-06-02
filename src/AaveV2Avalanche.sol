905653d0c5140403d1a9740bcab0273ae604cc94
eboado <ebdmrr@gmail.com>
2023-06-02 12:47:54 +0200

    feat: added v2 config engine and v2 rates factories

--- src/AaveV2Avalanche.sol
+++ src/AaveV2Avalanche.sol
@@ -43,6 +43,10 @@ library AaveV2Avalanche {
   address internal constant REPAY_WITH_COLLATERAL_ADAPTER =
     0x935b362EE3E1f342cc48118C528AAbee5118F6e6;
 
+  address internal constant LISTING_ENGINE = 0x7902F3c60f05b5A6b7e4Ce0Cac11Cb17bC8e607c;
+
+  address internal constant RATES_FACTORY = 0x6e66E50870A93691C1b953788A3219e01fDdeDD7;
+
   address internal constant MIGRATION_HELPER = 0xf50a080aC535e531EC33cC05b227E910De2fb1fA;
 
   address internal constant WALLET_BALANCE_PROVIDER = 0x73e4898a1Bfa9f710B6A6AB516403A6299e01fc6;
--- src/AaveV2EthereumAMM.sol
+++ src/AaveV2EthereumAMM.sol
@@ -38,6 +38,10 @@ library AaveV2EthereumAMM {
 
   address internal constant WETH_GATEWAY = 0x1C4a4e31231F71Fc34867D034a9E68f6fC798249;
 
+  address internal constant LISTING_ENGINE = 0xcfC26009618ec2Ca8787180116a37Caa354a465C;
+
+  address internal constant RATES_FACTORY = 0x6e4D068105052C3877116DCF86f5FF36B7eCa2B8;
+
   address internal constant WALLET_BALANCE_PROVIDER = 0x8E8dAd5409E0263a51C0aB5055dA66Be28cFF922;
 
   address internal constant UI_POOL_DATA_PROVIDER = 0x00e50FAB64eBB37b87df06Aa46b8B35d5f1A4e1A;
