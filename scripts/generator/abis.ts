bff33fc65520796e2589554f0fd72c6aa8da93d0
Lukas <lukasstrassel@googlemail.com>
2023-08-31 16:00:58 +0200

    feat: add abis to js exports (#229)
    * feat: add abis to js exports
    
    * fix: add generated comment

---
+++ scripts/generator/abis.ts
@@ -0,0 +1,39 @@
+import util from 'node:util';
+import {exec} from 'node:child_process';
+import {existsSync, mkdirSync, rmSync, writeFileSync} from 'node:fs';
+import {prefixWithGeneratedWarning} from './utils';
+
+const awaitableExec = util.promisify(exec);
+
+const INTERFACES = ['IAaveGovernanceV2', 'ICollector', 'AggregatorInterface'];
+
+export async function generateABIs() {
+  if (existsSync('./src/ts/abis')) {
+    rmSync('./src/ts/abis', {recursive: true});
+  }
+  mkdirSync('./src/ts/abis');
+  const jsExports = await Promise.all(
+    INTERFACES.map(async (INTERFACE) => {
+      const {stdout, stderr} = await awaitableExec(`forge inspect ${INTERFACE} abi`);
+      if (stderr) {
+        throw new Error(`Failed to generate abi for ${INTERFACE}`);
+      }
+      const varName = `${INTERFACE}_ABI`;
+      writeFileSync(
+        `./src/ts/abis/${INTERFACE}.ts`,
+        prefixWithGeneratedWarning(
+          `export const ${varName} = ${JSON.stringify(
+            JSON.parse(stdout.trim()),
+            null,
+            2,
+          )} as const;`,
+        ),
+      );
+      return `export {${varName}} from './abis/${INTERFACE}';`;
+    }),
+  );
+  return {
+    solidity: [],
+    js: jsExports,
+  };
+}
