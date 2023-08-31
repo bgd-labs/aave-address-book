5f186b26f67cb7ca0921b75bb3d16165a150889a
Mark Grothe <grothem@gmail.com>
2023-08-31 08:34:23 -0500

    fix: generate

--- src/ts/AaveV3Polygon.ts
+++ src/ts/AaveV3Polygon.ts
@@ -77,4 +77,7 @@ export const WALLET_BALANCE_PROVIDER = '0xBc790382B3686abffE4be14A030A96aC615402
 // https://polygonscan.com/address/0x1e4b7A6b903680eab0c5dAbcb8fD429cD2a9598c
 export const WETH_GATEWAY = '0x1e4b7A6b903680eab0c5dAbcb8fD429cD2a9598c';
 
+// https://polygonscan.com/address/0x78F8Bd884C3D738B74B420540659c82f392820e0
+export const WITHDRAW_SWAP_ADAPTER = '0x78F8Bd884C3D738B74B420540659c82f392820e0';
+
 export const CHAIN_ID = 137;
