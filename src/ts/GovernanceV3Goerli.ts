18fd39ba06af48d7e626bbb27176916821932405
sendra <polsendra@gmail.com>
2023-09-13 12:11:02 +0200

    fix: added goerli gov v3 contract helpers

--- src/ts/GovernanceV3Goerli.ts
+++ src/ts/GovernanceV3Goerli.ts
@@ -10,6 +10,18 @@ export const PAYLOADS_CONTROLLER = '0x064361B3761CcDd17300146bf58a79d1E570382E';
 // https://goerli.etherscan.io/address/0xFf376b6db4AF0d87Dba35860B3B87F526d7879cF
 export const VOTING_PORTAL_ETH_ETH = '0xFf376b6db4AF0d87Dba35860B3B87F526d7879cF';
 
+// https://goerli.etherscan.io/address/0xe10617A1ea760E174E82fBeb38a540d8ACe566f5
+export const VM_DATA_HELPER = '0xe10617A1ea760E174E82fBeb38a540d8ACe566f5';
+
+// https://goerli.etherscan.io/address/0xbd2db358f3C82F2883132A6584e22F38A979e768
+export const PC_DATA_HELPER = '0xbd2db358f3C82F2883132A6584e22F38A979e768';
+
+// https://goerli.etherscan.io/address/0x1966133c190475E8385Dc1b4150B5f81c70DC578
+export const META_DELEGATE_HELPER = '0x1966133c190475E8385Dc1b4150B5f81c70DC578';
+
+// https://goerli.etherscan.io/address/0x160e2d1456B815d6a3d281218538dd6E2e3C841f
+export const GOV_DATA_HELPER = '0x160e2d1456B815d6a3d281218538dd6E2e3C841f';
+
 // https://goerli.etherscan.io/address/0xb7Ea453180e3c7856b341199269c6ca78658c2b2
 export const EXECUTOR_LVL_1 = '0xb7Ea453180e3c7856b341199269c6ca78658c2b2';
 
