f6121e96764ae8dbb9e79b6df231a22cf7832edd
sendra <sendra@users.noreply.github.com>
2023-09-13 12:12:23 +0200

    fix: added missing gov v3 networks to generator (#244)

--- src/GovernanceV3Goerli.sol
+++ src/GovernanceV3Goerli.sol
@@ -19,6 +19,18 @@ library GovernanceV3Goerli {
   // https://goerli.etherscan.io/address/0xFf376b6db4AF0d87Dba35860B3B87F526d7879cF
   address internal constant VOTING_PORTAL_ETH_ETH = 0xFf376b6db4AF0d87Dba35860B3B87F526d7879cF;
 
+  // https://goerli.etherscan.io/address/0xe10617A1ea760E174E82fBeb38a540d8ACe566f5
+  address internal constant VM_DATA_HELPER = 0xe10617A1ea760E174E82fBeb38a540d8ACe566f5;
+
+  // https://goerli.etherscan.io/address/0xbd2db358f3C82F2883132A6584e22F38A979e768
+  address internal constant PC_DATA_HELPER = 0xbd2db358f3C82F2883132A6584e22F38A979e768;
+
+  // https://goerli.etherscan.io/address/0x1966133c190475E8385Dc1b4150B5f81c70DC578
+  address internal constant META_DELEGATE_HELPER = 0x1966133c190475E8385Dc1b4150B5f81c70DC578;
+
+  // https://goerli.etherscan.io/address/0x160e2d1456B815d6a3d281218538dd6E2e3C841f
+  address internal constant GOV_DATA_HELPER = 0x160e2d1456B815d6a3d281218538dd6E2e3C841f;
+
   // https://goerli.etherscan.io/address/0xb7Ea453180e3c7856b341199269c6ca78658c2b2
   address internal constant EXECUTOR_LVL_1 = 0xb7Ea453180e3c7856b341199269c6ca78658c2b2;
 
---
+++ src/ts/GovernanceV3Metis.ts
@@ -0,0 +1,14 @@
+// https://andromeda-explorer.metis.io/address/0x6fDaFb26915ABD6065a1E1501a37Ac438D877f70
+export const CROSS_CHAIN_CONTROLLER = '0x6fDaFb26915ABD6065a1E1501a37Ac438D877f70';
+
+// IPayloadsControllerCore https://andromeda-explorer.metis.io/address/0x2233F8A66A728FBa6E1dC95570B25360D07D5524
+export const PAYLOADS_CONTROLLER = '0x2233F8A66A728FBa6E1dC95570B25360D07D5524';
+
+// https://andromeda-explorer.metis.io/address/0x81d32B36380e6266e1BDd490eAC56cdB300afBe0
+export const PC_DATA_HELPER = '0x81d32B36380e6266e1BDd490eAC56cdB300afBe0';
+
+// https://andromeda-explorer.metis.io/address/0x6fD45D32375d5aDB8D76275A3932c740F03a8718
+export const EXECUTOR_LVL_1 = '0x6fD45D32375d5aDB8D76275A3932c740F03a8718';
+
+// https://andromeda-explorer.metis.io/address/0x0000000000000000000000000000000000000000
+export const EXECUTOR_LVL_2 = '0x0000000000000000000000000000000000000000';
