49ae2a3c76c71dce897bd4695807ce895b31f927
sakulstra <lukasstrassel@googlemail.com>
2023-09-19 23:30:22 +0200

    fix: update governancev2 links

--- src/AaveGovernanceV2.sol
+++ src/AaveGovernanceV2.sol
@@ -23,36 +23,36 @@ library AaveGovernanceV2 {
   // https://etherscan.io/address/0xAce1d11d836cb3F51Ef658FD4D353fFb3c301218
   address internal constant ARC_TIMELOCK = 0xAce1d11d836cb3F51Ef658FD4D353fFb3c301218;
 
-  // https://etherscan.io/address/0xdc9A35B16DB4e126cFeDC41322b3a36454B1F772
+  // https://polygonscan.com/address/0xdc9A35B16DB4e126cFeDC41322b3a36454B1F772
   address internal constant POLYGON_BRIDGE_EXECUTOR = 0xdc9A35B16DB4e126cFeDC41322b3a36454B1F772;
 
-  // https://etherscan.io/address/0x7d9103572bE58FfE99dc390E8246f02dcAe6f611
+  // https://explorer.optimism.io/address/0x7d9103572bE58FfE99dc390E8246f02dcAe6f611
   address internal constant OPTIMISM_BRIDGE_EXECUTOR = 0x7d9103572bE58FfE99dc390E8246f02dcAe6f611;
 
-  // https://etherscan.io/address/0x7d9103572bE58FfE99dc390E8246f02dcAe6f611
+  // https://arbiscan.io/address/0x7d9103572bE58FfE99dc390E8246f02dcAe6f611
   address internal constant ARBITRUM_BRIDGE_EXECUTOR = 0x7d9103572bE58FfE99dc390E8246f02dcAe6f611;
 
-  // https://etherscan.io/address/0x8EC77963068474a45016938Deb95E603Ca82a029
+  // https://andromeda-explorer.metis.io/address/0x8EC77963068474a45016938Deb95E603Ca82a029
   address internal constant METIS_BRIDGE_EXECUTOR = 0x8EC77963068474a45016938Deb95E603Ca82a029;
 
-  // https://etherscan.io/address/0xA9F30e6ED4098e9439B2ac8aEA2d3fc26BcEbb45
+  // https://basescan.org/address/0xA9F30e6ED4098e9439B2ac8aEA2d3fc26BcEbb45
   address internal constant BASE_BRIDGE_EXECUTOR = 0xA9F30e6ED4098e9439B2ac8aEA2d3fc26BcEbb45;
 
-  // https://polygonscan.com/address/0x158a6bC04F0828318821baE797f50B0A1299d45b
+  // https://etherscan.io/address/0x158a6bC04F0828318821baE797f50B0A1299d45b
   address internal constant CROSSCHAIN_FORWARDER_POLYGON =
     0x158a6bC04F0828318821baE797f50B0A1299d45b;
 
-  // https://explorer.optimism.io/address/0x5f5C02875a8e9B5A26fbd09040ABCfDeb2AA6711
+  // https://etherscan.io/address/0x5f5C02875a8e9B5A26fbd09040ABCfDeb2AA6711
   address internal constant CROSSCHAIN_FORWARDER_OPTIMISM =
     0x5f5C02875a8e9B5A26fbd09040ABCfDeb2AA6711;
 
-  // https://arbiscan.io/address/0xd1B3E25fD7C8AE7CADDC6F71b461b79CD4ddcFa3
+  // https://etherscan.io/address/0xd1B3E25fD7C8AE7CADDC6F71b461b79CD4ddcFa3
   address internal constant CROSSCHAIN_FORWARDER_ARBITRUM =
     0xd1B3E25fD7C8AE7CADDC6F71b461b79CD4ddcFa3;
 
-  // https://andromeda-explorer.metis.io/address/0x2fE52eF191F0BE1D98459BdaD2F1d3160336C08f
+  // https://etherscan.io/address/0x2fE52eF191F0BE1D98459BdaD2F1d3160336C08f
   address internal constant CROSSCHAIN_FORWARDER_METIS = 0x2fE52eF191F0BE1D98459BdaD2F1d3160336C08f;
 
-  // https://basescan.org/address/0x3215225538da1546FE0DA88ee13019f402078942
+  // https://etherscan.io/address/0x3215225538da1546FE0DA88ee13019f402078942
   address internal constant CROSSCHAIN_FORWARDER_BASE = 0x3215225538da1546FE0DA88ee13019f402078942;
 }
--- src/ts/AaveGovernanceV2.ts
+++ src/ts/AaveGovernanceV2.ts
@@ -14,32 +14,32 @@ export const LONG_EXECUTOR = '0x79426A1c24B2978D90d7A5070a46C65B07bC4299';
 // https://etherscan.io/address/0xAce1d11d836cb3F51Ef658FD4D353fFb3c301218
 export const ARC_TIMELOCK = '0xAce1d11d836cb3F51Ef658FD4D353fFb3c301218';
 
-// https://etherscan.io/address/0xdc9A35B16DB4e126cFeDC41322b3a36454B1F772
+// address https://polygonscan.com/address/0xdc9A35B16DB4e126cFeDC41322b3a36454B1F772
 export const POLYGON_BRIDGE_EXECUTOR = '0xdc9A35B16DB4e126cFeDC41322b3a36454B1F772';
 
-// https://etherscan.io/address/0x7d9103572bE58FfE99dc390E8246f02dcAe6f611
+// address https://explorer.optimism.io/address/0x7d9103572bE58FfE99dc390E8246f02dcAe6f611
 export const OPTIMISM_BRIDGE_EXECUTOR = '0x7d9103572bE58FfE99dc390E8246f02dcAe6f611';
 
-// https://etherscan.io/address/0x7d9103572bE58FfE99dc390E8246f02dcAe6f611
+// address https://arbiscan.io/address/0x7d9103572bE58FfE99dc390E8246f02dcAe6f611
 export const ARBITRUM_BRIDGE_EXECUTOR = '0x7d9103572bE58FfE99dc390E8246f02dcAe6f611';
 
-// https://etherscan.io/address/0x8EC77963068474a45016938Deb95E603Ca82a029
+// address https://andromeda-explorer.metis.io/address/0x8EC77963068474a45016938Deb95E603Ca82a029
 export const METIS_BRIDGE_EXECUTOR = '0x8EC77963068474a45016938Deb95E603Ca82a029';
 
-// https://etherscan.io/address/0xA9F30e6ED4098e9439B2ac8aEA2d3fc26BcEbb45
+// address https://basescan.org/address/0xA9F30e6ED4098e9439B2ac8aEA2d3fc26BcEbb45
 export const BASE_BRIDGE_EXECUTOR = '0xA9F30e6ED4098e9439B2ac8aEA2d3fc26BcEbb45';
 
-// address https://polygonscan.com/address/0x158a6bC04F0828318821baE797f50B0A1299d45b
+// https://etherscan.io/address/0x158a6bC04F0828318821baE797f50B0A1299d45b
 export const CROSSCHAIN_FORWARDER_POLYGON = '0x158a6bC04F0828318821baE797f50B0A1299d45b';
 
-// address https://explorer.optimism.io/address/0x5f5C02875a8e9B5A26fbd09040ABCfDeb2AA6711
+// https://etherscan.io/address/0x5f5C02875a8e9B5A26fbd09040ABCfDeb2AA6711
 export const CROSSCHAIN_FORWARDER_OPTIMISM = '0x5f5C02875a8e9B5A26fbd09040ABCfDeb2AA6711';
 
-// address https://arbiscan.io/address/0xd1B3E25fD7C8AE7CADDC6F71b461b79CD4ddcFa3
+// https://etherscan.io/address/0xd1B3E25fD7C8AE7CADDC6F71b461b79CD4ddcFa3
 export const CROSSCHAIN_FORWARDER_ARBITRUM = '0xd1B3E25fD7C8AE7CADDC6F71b461b79CD4ddcFa3';
 
-// address https://andromeda-explorer.metis.io/address/0x2fE52eF191F0BE1D98459BdaD2F1d3160336C08f
+// https://etherscan.io/address/0x2fE52eF191F0BE1D98459BdaD2F1d3160336C08f
 export const CROSSCHAIN_FORWARDER_METIS = '0x2fE52eF191F0BE1D98459BdaD2F1d3160336C08f';
 
-// address https://basescan.org/address/0x3215225538da1546FE0DA88ee13019f402078942
+// https://etherscan.io/address/0x3215225538da1546FE0DA88ee13019f402078942
 export const CROSSCHAIN_FORWARDER_BASE = '0x3215225538da1546FE0DA88ee13019f402078942';
