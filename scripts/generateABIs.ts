452f760d4133d1aba15434beaaaa8b3b3e71b457
Lukas <lukasstrassel@googlemail.com>
2023-10-18 14:29:42 +0200

    feat: gov v3-abis (#268)
    * feat: gov v3-abis
    
    * fix: fix ci

--- scripts/generateABIs.ts
+++ scripts/generateABIs.ts
@@ -2,7 +2,7 @@ import util from 'node:util';
 import {exec} from 'node:child_process';
 import {existsSync, mkdirSync, rmSync, writeFileSync} from 'node:fs';
 import {prefixWithGeneratedWarning} from './generator/utils';
-import {ABI_INTERFACES} from './configs/abis';
+import {ABI_INTERFACES, DOWNLOAD_ABI_INTERFACES} from './configs/abis';
 
 const awaitableExec = util.promisify(exec);
 
@@ -19,6 +19,19 @@ export async function generateABIs() {
     const varName = `${INTERFACE}_ABI`;
     writeFileSync(
       `./src/ts/abis/${INTERFACE}.ts`,
+      prefixWithGeneratedWarning(
+        `export const ${varName} = ${JSON.stringify(JSON.parse(stdout.trim()), null, 2)} as const;`,
+      ),
+    );
+  }
+  for (const INTERFACE of DOWNLOAD_ABI_INTERFACES) {
+    const {stdout, stderr} = await awaitableExec(`cast interface -j ${INTERFACE.address}`);
+    if (stderr) {
+      throw new Error(`Failed to generate abi for ${INTERFACE.name} from ${INTERFACE.address}`);
+    }
+    const varName = `${INTERFACE.name}_ABI`;
+    writeFileSync(
+      `./src/ts/abis/${INTERFACE.name}.ts`,
       prefixWithGeneratedWarning(
         `export const ${varName} = ${JSON.stringify(JSON.parse(stdout.trim()), null, 2)} as const;`,
       ),
--- scripts/generator/abis.ts
+++ scripts/generator/abis.ts
@@ -1,10 +1,14 @@
-import {ABI_INTERFACES} from '../configs/abis';
+import {ABI_INTERFACES, DOWNLOAD_ABI_INTERFACES} from '../configs/abis';
 
 export function generateABIImports() {
   const jsExports: string[] = [];
   for (const INTERFACE of ABI_INTERFACES) {
     const varName = `${INTERFACE}_ABI`;
     jsExports.push(`export {${varName}} from './abis/${INTERFACE}';`);
+  }
+  for (const INTERFACE of DOWNLOAD_ABI_INTERFACES) {
+    const varName = `${INTERFACE.name}_ABI`;
+    jsExports.push(`export {${varName}} from './abis/${INTERFACE.name}';`);
   }
   return {
     solidity: [],
