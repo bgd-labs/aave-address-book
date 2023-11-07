950cd2aed7fa2300c60b9683caf6caf5244e47cf
sakulstra <sakulstra@users.noreply.github.com>
2023-11-07 11:01:04 +0000

    chore(cache): automated cache update

--- src/AaveV3Gnosis.sol
+++ src/AaveV3Gnosis.sol
@@ -87,6 +87,156 @@ library AaveV3Gnosis {
   address internal constant FREEZING_STEWARD = 0x3Ceaf9b6CAb92dFe6302D0CC3F1BA880C28d35e5;
 }
 
-library AaveV3GnosisAssets {}
+library AaveV3GnosisAssets {
+  // https://blockscout.com/xdai/mainnet/address/0x6A023CCd1ff6F2045C3309768eAd9E68F978f6e1
+  address internal constant WETH_UNDERLYING = 0x6A023CCd1ff6F2045C3309768eAd9E68F978f6e1;
 
-library AaveV3GnosisEModes {}
+  uint256 internal constant WETH_DECIMALS = 18;
+
+  // https://blockscout.com/xdai/mainnet/address/0xa818F1B57c201E092C4A2017A91815034326Efd1
+  address internal constant WETH_A_TOKEN = 0xa818F1B57c201E092C4A2017A91815034326Efd1;
+
+  // https://blockscout.com/xdai/mainnet/address/0x0c0fce05F2314540EcB095bF4D069e5E0ED90fF8
+  address internal constant WETH_V_TOKEN = 0x0c0fce05F2314540EcB095bF4D069e5E0ED90fF8;
+
+  // https://blockscout.com/xdai/mainnet/address/0x436D82d905b014926a2375C576500B6fea0D2496
+  address internal constant WETH_S_TOKEN = 0x436D82d905b014926a2375C576500B6fea0D2496;
+
+  // https://blockscout.com/xdai/mainnet/address/0xa767f745331D267c7751297D982b050c93985627
+  address internal constant WETH_ORACLE = 0xa767f745331D267c7751297D982b050c93985627;
+
+  // https://blockscout.com/xdai/mainnet/address/0xD84d86083010FB683f1e8fA3809ee8DC90A4C4DB
+  address internal constant WETH_INTEREST_RATE_STRATEGY =
+    0xD84d86083010FB683f1e8fA3809ee8DC90A4C4DB;
+
+  // https://blockscout.com/xdai/mainnet/address/0x6C76971f98945AE98dD7d4DFcA8711ebea946eA6
+  address internal constant wstETH_UNDERLYING = 0x6C76971f98945AE98dD7d4DFcA8711ebea946eA6;
+
+  uint256 internal constant wstETH_DECIMALS = 18;
+
+  // https://blockscout.com/xdai/mainnet/address/0x23e4E76D01B2002BE436CE8d6044b0aA2f68B68a
+  address internal constant wstETH_A_TOKEN = 0x23e4E76D01B2002BE436CE8d6044b0aA2f68B68a;
+
+  // https://blockscout.com/xdai/mainnet/address/0x9D881f67F20B49243c98f53d2B9E91E39d02Ae09
+  address internal constant wstETH_V_TOKEN = 0x9D881f67F20B49243c98f53d2B9E91E39d02Ae09;
+
+  // https://blockscout.com/xdai/mainnet/address/0x5CBc43c339F5202D2dcB59583D33cA8498b75031
+  address internal constant wstETH_S_TOKEN = 0x5CBc43c339F5202D2dcB59583D33cA8498b75031;
+
+  // https://blockscout.com/xdai/mainnet/address/0xcb0670258e5961CCA85D8F71D29C1167Ef20De99
+  address internal constant wstETH_ORACLE = 0xcb0670258e5961CCA85D8F71D29C1167Ef20De99;
+
+  // https://blockscout.com/xdai/mainnet/address/0x9E57695Dab0DCdb42BC220ff1E9eb2e22a31209b
+  address internal constant wstETH_INTEREST_RATE_STRATEGY =
+    0x9E57695Dab0DCdb42BC220ff1E9eb2e22a31209b;
+
+  // https://blockscout.com/xdai/mainnet/address/0x9C58BAcC331c9aa871AFD802DB6379a98e80CEdb
+  address internal constant GNO_UNDERLYING = 0x9C58BAcC331c9aa871AFD802DB6379a98e80CEdb;
+
+  uint256 internal constant GNO_DECIMALS = 18;
+
+  // https://blockscout.com/xdai/mainnet/address/0xA1Fa064A85266E2Ca82DEe5C5CcEC84DF445760e
+  address internal constant GNO_A_TOKEN = 0xA1Fa064A85266E2Ca82DEe5C5CcEC84DF445760e;
+
+  // https://blockscout.com/xdai/mainnet/address/0xBc59E99198DbA71985A66E1713cC89FFEC53f7FC
+  address internal constant GNO_V_TOKEN = 0xBc59E99198DbA71985A66E1713cC89FFEC53f7FC;
+
+  // https://blockscout.com/xdai/mainnet/address/0x1A126F613D7705E59ADb39909b25E1223aDF05dd
+  address internal constant GNO_S_TOKEN = 0x1A126F613D7705E59ADb39909b25E1223aDF05dd;
+
+  // https://blockscout.com/xdai/mainnet/address/0x22441d81416430A54336aB28765abd31a792Ad37
+  address internal constant GNO_ORACLE = 0x22441d81416430A54336aB28765abd31a792Ad37;
+
+  // https://blockscout.com/xdai/mainnet/address/0x9E57695Dab0DCdb42BC220ff1E9eb2e22a31209b
+  address internal constant GNO_INTEREST_RATE_STRATEGY = 0x9E57695Dab0DCdb42BC220ff1E9eb2e22a31209b;
+
+  // https://blockscout.com/xdai/mainnet/address/0xDDAfbb505ad214D7b80b1f830fcCc89B60fb7A83
+  address internal constant USDC_UNDERLYING = 0xDDAfbb505ad214D7b80b1f830fcCc89B60fb7A83;
+
+  uint256 internal constant USDC_DECIMALS = 6;
+
+  // https://blockscout.com/xdai/mainnet/address/0xc6B7AcA6DE8a6044E0e32d0c841a89244A10D284
+  address internal constant USDC_A_TOKEN = 0xc6B7AcA6DE8a6044E0e32d0c841a89244A10D284;
+
+  // https://blockscout.com/xdai/mainnet/address/0x5F6f7B0a87CA3CF3d0b431Ae03EF3305180BFf4d
+  address internal constant USDC_V_TOKEN = 0x5F6f7B0a87CA3CF3d0b431Ae03EF3305180BFf4d;
+
+  // https://blockscout.com/xdai/mainnet/address/0x8220133C3a631de3C7A5B679A2417BD61431FEcf
+  address internal constant USDC_S_TOKEN = 0x8220133C3a631de3C7A5B679A2417BD61431FEcf;
+
+  // https://blockscout.com/xdai/mainnet/address/0x26C31ac71010aF62E6B486D1132E266D6298857D
+  address internal constant USDC_ORACLE = 0x26C31ac71010aF62E6B486D1132E266D6298857D;
+
+  // https://blockscout.com/xdai/mainnet/address/0x345f7fcd88207fe060AD03c2656A23A3Ab596479
+  address internal constant USDC_INTEREST_RATE_STRATEGY =
+    0x345f7fcd88207fe060AD03c2656A23A3Ab596479;
+
+  // https://blockscout.com/xdai/mainnet/address/0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d
+  address internal constant WXDAI_UNDERLYING = 0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d;
+
+  uint256 internal constant WXDAI_DECIMALS = 18;
+
+  // https://blockscout.com/xdai/mainnet/address/0xd0Dd6cEF72143E22cCED4867eb0d5F2328715533
+  address internal constant WXDAI_A_TOKEN = 0xd0Dd6cEF72143E22cCED4867eb0d5F2328715533;
+
+  // https://blockscout.com/xdai/mainnet/address/0x281963D7471eCdC3A2Bd4503e24e89691cfe420D
+  address internal constant WXDAI_V_TOKEN = 0x281963D7471eCdC3A2Bd4503e24e89691cfe420D;
+
+  // https://blockscout.com/xdai/mainnet/address/0xaC8b1cE0548C69318920C3e0b21Db296d5770D57
+  address internal constant WXDAI_S_TOKEN = 0xaC8b1cE0548C69318920C3e0b21Db296d5770D57;
+
+  // https://blockscout.com/xdai/mainnet/address/0x678df3415fc31947dA4324eC63212874be5a82f8
+  address internal constant WXDAI_ORACLE = 0x678df3415fc31947dA4324eC63212874be5a82f8;
+
+  // https://blockscout.com/xdai/mainnet/address/0x345f7fcd88207fe060AD03c2656A23A3Ab596479
+  address internal constant WXDAI_INTEREST_RATE_STRATEGY =
+    0x345f7fcd88207fe060AD03c2656A23A3Ab596479;
+
+  // https://blockscout.com/xdai/mainnet/address/0xcB444e90D8198415266c6a2724b7900fb12FC56E
+  address internal constant EURe_UNDERLYING = 0xcB444e90D8198415266c6a2724b7900fb12FC56E;
+
+  uint256 internal constant EURe_DECIMALS = 18;
+
+  // https://blockscout.com/xdai/mainnet/address/0xEdBC7449a9b594CA4E053D9737EC5Dc4CbCcBfb2
+  address internal constant EURe_A_TOKEN = 0xEdBC7449a9b594CA4E053D9737EC5Dc4CbCcBfb2;
+
+  // https://blockscout.com/xdai/mainnet/address/0xb96404e475f337A7E98e4a541C9b71309BB66c5A
+  address internal constant EURe_V_TOKEN = 0xb96404e475f337A7E98e4a541C9b71309BB66c5A;
+
+  // https://blockscout.com/xdai/mainnet/address/0x916E13857FeeD0d982Df148DBe8d8542519aB96e
+  address internal constant EURe_S_TOKEN = 0x916E13857FeeD0d982Df148DBe8d8542519aB96e;
+
+  // https://blockscout.com/xdai/mainnet/address/0xab70BCB260073d036d1660201e9d5405F5829b7a
+  address internal constant EURe_ORACLE = 0xab70BCB260073d036d1660201e9d5405F5829b7a;
+
+  // https://blockscout.com/xdai/mainnet/address/0x6c3b7e7B8b9609D57C70C3F630228F979EAbb450
+  address internal constant EURe_INTEREST_RATE_STRATEGY =
+    0x6c3b7e7B8b9609D57C70C3F630228F979EAbb450;
+
+  // https://blockscout.com/xdai/mainnet/address/0xaf204776c7245bF4147c2612BF6e5972Ee483701
+  address internal constant sDAI_UNDERLYING = 0xaf204776c7245bF4147c2612BF6e5972Ee483701;
+
+  uint256 internal constant sDAI_DECIMALS = 18;
+
+  // https://blockscout.com/xdai/mainnet/address/0x7a5c3860a77a8DC1b225BD46d0fb2ac1C6D191BC
+  address internal constant sDAI_A_TOKEN = 0x7a5c3860a77a8DC1b225BD46d0fb2ac1C6D191BC;
+
+  // https://blockscout.com/xdai/mainnet/address/0x8Fe06E1D8Aff42Bf6812CacF7854A2249a00bED7
+  address internal constant sDAI_V_TOKEN = 0x8Fe06E1D8Aff42Bf6812CacF7854A2249a00bED7;
+
+  // https://blockscout.com/xdai/mainnet/address/0xa2E0335175Da40b081717FfD394C0E1de738cb9B
+  address internal constant sDAI_S_TOKEN = 0xa2E0335175Da40b081717FfD394C0E1de738cb9B;
+
+  // https://blockscout.com/xdai/mainnet/address/0x1D0f881Ce1a646E2f27Dec3c57Fa056cB838BCC2
+  address internal constant sDAI_ORACLE = 0x1D0f881Ce1a646E2f27Dec3c57Fa056cB838BCC2;
+
+  // https://blockscout.com/xdai/mainnet/address/0x6c3b7e7B8b9609D57C70C3F630228F979EAbb450
+  address internal constant sDAI_INTEREST_RATE_STRATEGY =
+    0x6c3b7e7B8b9609D57C70C3F630228F979EAbb450;
+}
+
+library AaveV3GnosisEModes {
+  uint8 internal constant NONE = 0;
+
+  uint8 internal constant ETH_CORRELATED = 1;
+}
