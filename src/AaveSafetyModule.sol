3b7770b6c78a291367a21e375577f08c44e3f07b
sakulstra <lukasstrassel@googlemail.com>
2023-11-08 16:36:40 +0100

    fix: update generated files

--- src/AaveSafetyModule.sol
+++ src/AaveSafetyModule.sol
@@ -8,4 +8,10 @@ library AaveSafetyModule {
 
   // https://etherscan.io/address/0xa1116930326D21fB917d5A27F1E9943A9595fb47
   address internal constant STK_ABPT = 0xa1116930326D21fB917d5A27F1E9943A9595fb47;
+
+  // https://etherscan.io/address/0x209Ad99bd808221293d03827B86cC544bcA0023b
+  address internal constant STK_ABPT_ORACLE = 0x209Ad99bd808221293d03827B86cC544bcA0023b;
+
+  // https://etherscan.io/address/0xADf86b537eF08591c2777E144322E8b0Ca7E82a7
+  address internal constant STK_ABPT_V2_ORACLE = 0xADf86b537eF08591c2777E144322E8b0Ca7E82a7;
 }
