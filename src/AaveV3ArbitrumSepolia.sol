beb024cb859e05cf6a1cb2ffa4fca4de5640a5ea
Parth Patel <parth4321patel@gmail.com>
2024-01-17 15:15:30 +0530

    add Gho token for arbitrum sepolia

---
+++ src/AaveV3ArbitrumSepolia.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0;
+
+library AaveV3Sepolia {
+  // https://sepolia.arbiscan.io/address/0xb13cfa6f8b2eed2c37fb00ff0c1a59807c585810
+  address internal constant GHO_TOKEN = 0xb13Cfa6f8B2Eed2C37fB00fF0c1A59807C585810;
+}
+
+library AaveV3SepoliaAssets {
+  // https://sepolia.arbiscan.io/address/0xb13cfa6f8b2eed2c37fb00ff0c1a59807c585810
+  address internal constant GHO_UNDERLYING = 0xb13Cfa6f8B2Eed2C37fB00fF0c1A59807C585810;
+
+  uint8 internal constant GHO_DECIMALS = 18;
+}
