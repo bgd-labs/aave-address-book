036bf8764aeb8d81da61120d67c0a0d1e9ea690e
Parth Patel <parth4321patel@gmail.com>
2024-01-17 16:33:47 +0530

    undo removal of asset and remove AaveV3ArbitrumSepoliaAssets lib

--- src/AaveV3ArbitrumSepolia.sol
+++ src/AaveV3ArbitrumSepolia.sol
@@ -5,10 +5,3 @@ library AaveV3ArbitrumSepolia {
   // https://sepolia.arbiscan.io/address/0xb13cfa6f8b2eed2c37fb00ff0c1a59807c585810
   address internal constant GHO_TOKEN = 0xb13Cfa6f8B2Eed2C37fB00fF0c1A59807C585810;
 }
-
-library AaveV3ArbitrumSepoliaAssets {
-  // https://sepolia.arbiscan.io/address/0xb13cfa6f8b2eed2c37fb00ff0c1a59807c585810
-  address internal constant GHO_UNDERLYING = 0xb13Cfa6f8B2Eed2C37fB00fF0c1A59807C585810;
-
-  uint8 internal constant GHO_DECIMALS = 18;
-}
