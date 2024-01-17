b9c174b5686de712311602e35cd98581d10c8a68
Parth Patel <parth4321patel@gmail.com>
2024-01-17 22:37:14 +0530

    create MiscArbitrumSepolia

--- src/MiscArbitrumSepolia.sol
+++ src/MiscArbitrumSepolia.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0;
 
-library AaveV3ArbitrumSepolia {
+library MiscArbitrumSepolia {
   // https://sepolia.arbiscan.io/address/0xb13cfa6f8b2eed2c37fb00ff0c1a59807c585810
   address internal constant GHO_TOKEN = 0xb13Cfa6f8B2Eed2C37fB00fF0c1A59807C585810;
 }
