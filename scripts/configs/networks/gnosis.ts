694ccf75ab11e105f7601599f51a6c15a09dd481
Andrei Kozlov <kyzia.ru@gmail.com>
2024-02-02 18:57:20 +0700

    update generator

--- scripts/configs/networks/gnosis.ts
+++ scripts/configs/networks/gnosis.ts
@@ -1,7 +1,8 @@
+import {Hex} from 'viem';
 import {ChainId} from '@bgd-labs/js-utils';
 import {NetworkAddresses} from '../types';
 
-export const gnosisAddresses: NetworkAddresses = {
+export const gnosisAddresses: NetworkAddresses<{wstETH_stETH_AGGREGATOR: Hex}> = {
   name: 'Gnosis',
   chainId: ChainId.gnosis,
   addresses: {
@@ -9,5 +10,6 @@ export const gnosisAddresses: NetworkAddresses = {
     PROXY_ADMIN: '0xe892E40C92c2E4D281Be59b2E6300F271d824E75',
     CREATE_3_FACTORY: '0x2cC41488f4494c5e11cB6cbA82199442B9f495B6',
     PROTOCOL_GUARDIAN: '0xF163b8698821cefbD33Cf449764d69Ea445cE23D',
+    wstETH_stETH_AGGREGATOR: '0x0064AC007fF665CF8D0D3Af5E0AD1c26a3f853eA',
   },
 };
