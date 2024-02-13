0106ffe07d8a7692d06a3a3fca164f2b58eae3ea
K.K <m@mnm.mn>
2024-02-13 18:14:46 +0200

    Add search UI (#356)
    * feat: add basic search UI
    
    * feat: update icons & layout
    
    * feat: export chain_id for v2
    
    * chore: update readme
    
    * feat: update list item
    
    * chore: remove package-lock from gitignore
    
    * feat: add copy button
    
    * feat: update input focus state

---
+++ ui/src/app/page.tsx
@@ -0,0 +1,62 @@
+import * as addressBook from '../../../src/ts/AaveAddressBook';
+import { CHAIN_ID_CLIENT_MAP } from '@bgd-labs/js-utils';
+import { Search } from '@/components/Search';
+import { Footer } from '@/components/Footer';
+import { type Address } from '@/types';
+
+
+function isEthereumAddress(value: any): value is string {
+  return typeof value === 'string' && /^0x[a-fA-F0-9]{40}$/.test(value);
+}
+
+function flattenObject(
+  obj: any,
+  path: string[] = [],
+  chainId: number | null = null,
+): Address[] {
+  const result: Address[] = [];
+  const entries = Object.entries(obj).sort(([keyA], [keyB]) => {
+    if (keyA === 'CHAIN_ID') return -1;
+    if (keyB === 'CHAIN_ID') return 1;
+    return 0;
+  });
+
+  for (let [key, value] of entries) {
+    const newPath = [...path, key];
+    if (key === 'CHAIN_ID') {
+      chainId = value as number;
+    }
+    if (typeof value === 'object' && value !== null) {
+      result.push(...flattenObject(value, newPath, chainId));
+    } else if (isEthereumAddress(value)) {
+      const link = `${CHAIN_ID_CLIENT_MAP[chainId!]?.chain?.blockExplorers?.default.url}/address/${value}`;
+      result.push({
+        path: newPath,
+        value,
+        chainId,
+        link,
+        searchPath: newPath.join(' '),
+      });
+    }
+  }
+  return result;
+}
+
+const addresses = flattenObject(addressBook);
+
+export default function Home() {
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-start pl-4 pr-2 py-6 sm:py-10">
+        <h1 className="leading-4 text-center mb-5">
+          <div className="uppercase text-4xl font-heading font-bold">Aave</div>
+          <div className="text-[11px] uppercase font-semibold text-brand-500 tracking-wide">
+            Address Book
+          </div>
+        </h1>
+        <Search addresses={addresses} />
+        <Footer />
+      </main>
+    </>
+  );
+}
---
+++ ui/src/components/Search.tsx
@@ -0,0 +1,100 @@
+'use client';
+
+import { useState, useEffect, useCallback, useMemo, useRef } from 'react';
+import { cn } from '@/utils/cn';
+import { type Address } from '@/types';
+import Fuse, { FuseResult } from 'fuse.js';
+import { Box } from './Box';
+import { SearchResult } from './SearchResult';
+
+const fuseOptions = {
+  isCaseSensitive: false,
+  includeScore: false,
+  shouldSort: true,
+  includeMatches: false,
+  findAllMatches: false,
+  minMatchCharLength: 2,
+  location: 0,
+  threshold: 0.4,
+  distance: 100,
+  useExtendedSearch: false,
+  ignoreLocation: false,
+  ignoreFieldNorm: false,
+  fieldNormWeight: 1,
+  keys: [{ name: 'searchPath', weight: 3 }, 'value'],
+};
+
+const SEARCH_LIMIT = 32;
+const DEBOUNCE_TIME = 150;
+
+export const Search = ({ addresses }: { addresses: Address[] }) => {
+  const [search, setSearch] = useState('');
+  const [results, setResults] = useState<FuseResult<Address>[]>([]);
+  const timeoutId = useRef<ReturnType<typeof setInterval> | null>(null);
+
+  const fuseIndex = useMemo(
+    () => Fuse.createIndex(fuseOptions.keys, addresses),
+    [addresses],
+  );
+
+  const performSearch = useCallback(
+    (search: string) => {
+      const fuse = new Fuse(addresses, fuseOptions, fuseIndex);
+      if (search) {
+        const limitedSearch = search.slice(0, SEARCH_LIMIT);
+        setResults(fuse.search(limitedSearch, { limit: 100 }));
+      } else {
+        setResults([]);
+      }
+    },
+    [addresses, fuseIndex],
+  );
+
+  useEffect(() => {
+    if (timeoutId.current) {
+      clearTimeout(timeoutId.current);
+    }
+    timeoutId.current = setTimeout(() => {
+      performSearch(search);
+    }, DEBOUNCE_TIME);
+    return () => {
+      if (timeoutId.current !== null) {
+        clearTimeout(timeoutId.current);
+      }
+    };
+  }, [search, performSearch]);
+
+  return (
+    <div className="w-full max-w-2xl mb-10">
+      <Box
+        className={cn('group border-brand-900 border', {
+          ['border-b-0']: results.length > 0,
+        })}
+      >
+        <svg
+          className="w-7 h-7 group-focus-within:text-blue-500 absolute top-1/2 left-3 -translate-y-1/2 text-brand-500 transition-all"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="1.5"
+            d="M19.25 19.25L15.5 15.5M4.75 11C4.75 7.54822 7.54822 4.75 11 4.75C14.4518 4.75 17.25 7.54822 17.25 11C17.25 14.4518 14.4518 17.25 11 17.25C7.54822 17.25 4.75 14.4518 4.75 11Z"
+          ></path>
+        </svg>
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className="outline-none py-3 px-11 w-full text-xl border border-4 border-transparent focus:border-blue-100 transition-all ring-inset focus:ring-1	ring-blue-300"
+        />
+      </Box>
+      {results.length !== 0 &&
+        results.map((result) => (
+          <SearchResult key={result.item.searchPath} result={result} />
+        ))}
+    </div>
+  );
+};
