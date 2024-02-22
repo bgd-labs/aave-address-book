c969d6523280080a9828c9b937d66ecbf8b1f70c
sakulstra <lukasstrassel@googlemail.com>
2024-02-22 17:42:42 +0100

    fix: regenerate tokenlist

--- scripts/generator/svgUtils.ts
+++ scripts/generator/svgUtils.ts
@@ -10,34 +10,23 @@ export enum VARIANT {
   STATA_TOKEN,
 }
 
-export enum VARIANT_SUFFIX {
-  A_TOKEN = '_aToken',
-  STATA_TOKEN = '_stataToken',
+export enum VARIANT_FOLDER {
+  UNDERLYING = 'underlying',
+  A_TOKEN = 'aToken',
+  STATA_TOKEN = 'stataToken',
 }
 
-export async function getSymbolUri(symbol: string, variant: VARIANT): Promise<string | undefined> {
-  const fileName = getFileName(symbol, variant);
-  const exists = assets.includes(fileName);
+export async function getSymbolUri(_symbol: string, variant: VARIANT): Promise<string | undefined> {
+  const symbol = _symbol.toLowerCase();
+  const exists = assets.includes(symbol);
   if (exists) {
     const cid = await getHash(
-      readFileSync(join(process.cwd(), 'assets/underlying', fileName), {encoding: 'utf8'}),
+      readFileSync(join(process.cwd(), 'assets', VARIANT_FOLDER[variant], symbol), {
+        encoding: 'utf8',
+      }),
     );
     return `ipfs://${cid}`;
   }
-}
-
-/**
- * Returns the filename based on a constant pattern
- * @param _symbol
- * @param variant
- * @returns
- */
-function getFileName(_symbol: string, variant: VARIANT): string {
-  const symbol = _symbol.toLowerCase();
-  if (variant === VARIANT.UNDERLYING) return `${symbol}.svg`;
-  if (variant === VARIANT.A_TOKEN) return `${symbol}${VARIANT_SUFFIX.A_TOKEN}.svg`;
-  if (variant === VARIANT.STATA_TOKEN) return `${symbol}${VARIANT_SUFFIX.STATA_TOKEN}.svg`;
-  throw new Error('unknown variant');
 }
 
 /**
