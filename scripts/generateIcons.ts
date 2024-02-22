ad22fa51192a0555b7f590b41618f6c7205269d6
sakulstra <lukasstrassel@googlemail.com>
2024-02-22 16:22:33 +0100

    feat: add stata tokens

--- scripts/generateIcons.ts
+++ scripts/generateIcons.ts
@@ -1,6 +1,7 @@
 import {join} from 'path';
 import {VARIANT_SUFFIX, assets} from './generator/svgUtils';
 import {readFileSync, writeFileSync} from 'fs';
+import {XMLParser} from 'fast-xml-parser';
 
 const basePath = join(process.cwd(), 'assets');
 
@@ -13,8 +14,18 @@ async function main() {
   }
 }
 
+const stataTemplate = readFileSync(join(basePath, 'templates', 'stata.svg'), {
+  encoding: 'utf8',
+});
+
 function generateStataToken(fileName: string, underlying: string) {
-  writeFileSync(join(basePath, 'stataToken', fileName), underlying);
+  const svg = stataTemplate.replace(
+    '<template />',
+    underlying
+      .replace('height="48"', 'x="9" y="9" width="30" height="30"')
+      .replace('width="48"', ''),
+  );
+  writeFileSync(join(basePath, 'stataToken', fileName), svg);
 }
 
 function generateAToken() {}
