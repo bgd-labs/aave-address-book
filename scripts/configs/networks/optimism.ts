d6124b068788491cd2776f119187e9ffb7a5b9ff
Harsh Pandey <harshsatishpandey@gmail.com>
2024-03-08 22:40:46 +0530

    feat: add arbitrum sepolia (#396)

--- scripts/configs/networks/optimism.ts
+++ scripts/configs/networks/optimism.ts
@@ -19,3 +19,12 @@ export const optimismAddresses: NetworkAddresses<{
     rETH_ETH_AGGREGATOR: '0x22F3727be377781d1579B7C9222382b21c9d1a8f',
   },
 };
+
+export const optimismSepoliaAddresses: NetworkAddresses = {
+  name: 'OptimismSepolia',
+  chainId: ChainId.optimism_sepolia,
+  addresses: {
+    TRANSPARENT_PROXY_FACTORY: '0xb172a90A7C238969CE9B27cc19D13b60A91e7F00',
+    PROXY_ADMIN: '0xe892E40C92c2E4D281Be59b2E6300F271d824E75'
+  },
+};
