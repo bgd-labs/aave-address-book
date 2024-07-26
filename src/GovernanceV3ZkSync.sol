23fb03113179ca942ed4f5a5f91faf7120007505
sendra <polsendra@gmail.com>
2024-07-26 11:09:10 +0200

    fix: new addresses

--- src/GovernanceV3ZkSync.sol
+++ src/GovernanceV3ZkSync.sol
@@ -5,12 +5,12 @@ pragma solidity >=0.6.0;
 import {IGovernanceCore, IPayloadsControllerCore, IDataWarehouse, IVotingStrategy} from './GovernanceV3.sol';
 
 library GovernanceV3ZkSync {
-  // https://era.zksync.network//address/0xA707c95C9e9acB5168971E92b6152aeb0d853153
-  address internal constant CROSS_CHAIN_CONTROLLER = 0xA707c95C9e9acB5168971E92b6152aeb0d853153;
+  // https://era.zksync.network//address/0x800813f4714BC7A0a95310e3fB9e4f18872CA92C
+  address internal constant CROSS_CHAIN_CONTROLLER = 0x800813f4714BC7A0a95310e3fB9e4f18872CA92C;
 
-  // https://era.zksync.network//address/0x693E85a21287f3EA255E83bDf5e1192486251360
+  // https://era.zksync.network//address/0x2E79349c3F5e4751E87b966812C9E65E805996F1
   IPayloadsControllerCore internal constant PAYLOADS_CONTROLLER =
-    IPayloadsControllerCore(0x693E85a21287f3EA255E83bDf5e1192486251360);
+    IPayloadsControllerCore(0x2E79349c3F5e4751E87b966812C9E65E805996F1);
 
   // https://era.zksync.network//address/0xe28A3235DCF1Acb8397B546bd588bAAFD7081505
   address internal constant PC_DATA_HELPER = 0xe28A3235DCF1Acb8397B546bd588bAAFD7081505;
