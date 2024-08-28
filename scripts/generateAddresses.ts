2ac4bc267944a60e2823587ff6721943cb7521ce
kartojal <david@catapulta.sh>
2024-08-28 08:46:49 +0000

    feat: add Aave v3 EtherFi Ethereum addresses

--- scripts/generateAddresses.ts
+++ scripts/generateAddresses.ts
@@ -16,6 +16,7 @@ import {
   mainnetProtoV2Pool,
   sepoliaProtoV3,
   lidoEthereumMainnetProtoV3Pool,
+  etherFiEthereumMainnetProtoV3Pool,
 } from './configs/pools/ethereum';
 import {fantomProtoV3, fantomTestnetProtoV3} from './configs/pools/fantom';
 import {harmonyProtoV3} from './configs/pools/harmony';
@@ -128,6 +129,7 @@ async function main() {
       fantomProtoV3,
       harmonyProtoV3,
       lidoEthereumMainnetProtoV3Pool,
+      etherFiEthereumMainnetProtoV3Pool,
     ].map((config) => generateProtocolV3Library(config)),
   );
 
