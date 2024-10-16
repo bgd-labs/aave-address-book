c9ab73be76dd4f5f8684a2cdd6614acaae7856f4
Lukas <lukasstrassel@googlemail.com>
2024-10-16 08:36:14 +0200

    fix: readd tokenlist (#642)

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,14 +2,27 @@ import {defineConfig} from 'tsup';
 import {esbuildPluginFilePathExtensions} from 'esbuild-plugin-file-path-extensions';
 
 // https://github.com/egoist/tsup/issues/953
-export default defineConfig({
-  format: ['cjs', 'esm'],
-  entry: ['src/**/*.ts'],
-  outDir: './dist',
-  bundle: true,
-  sourcemap: true,
-  clean: true,
-  splitting: false,
-  dts: true,
-  esbuildPlugins: [esbuildPluginFilePathExtensions({cjsExtension: 'js'})],
-});
+export default defineConfig([
+  {
+    format: ['cjs', 'esm'],
+    entry: ['src/**/*.ts'],
+    outDir: './dist',
+    bundle: true,
+    sourcemap: true,
+    // clean: true,
+    splitting: false,
+    dts: true,
+    esbuildPlugins: [esbuildPluginFilePathExtensions({cjsExtension: 'js'})],
+  },
+  {
+    format: ['cjs', 'esm'],
+    entry: ['tokenlist.json'],
+    outDir: './dist',
+    bundle: true,
+    sourcemap: true,
+    // clean: true,
+    splitting: false,
+    dts: true,
+    esbuildPlugins: [esbuildPluginFilePathExtensions({cjsExtension: 'js'})],
+  },
+]);
