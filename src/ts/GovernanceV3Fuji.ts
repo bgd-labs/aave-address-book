b35492f63ad3e5cc4b0972b4cd6617e7f324d54c
Lukas <lukasstrassel@googlemail.com>
2025-03-10 08:42:47 +0100

    argh

---
+++ src/ts/GovernanceV3Fuji.ts
@@ -0,0 +1,19 @@
+// https://testnet.snowtrace.io/address/0x5488A48E5C22175548E8d68622211F5928cBbf23
+export const CROSS_CHAIN_CONTROLLER = '0x5488A48E5C22175548E8d68622211F5928cBbf23';
+
+// https://testnet.snowtrace.io/address/0x79Edd77c76F95b6b69907CaD1d8241F9DB0e3FD0
+export const VOTING_MACHINE = '0x79Edd77c76F95b6b69907CaD1d8241F9DB0e3FD0';
+
+// IPayloadsControllerCore https://testnet.snowtrace.io/address/0xf1842f9D6C3D9ec1153d7afCBb9cDBC537Ea5d15
+export const PAYLOADS_CONTROLLER = '0xf1842f9D6C3D9ec1153d7afCBb9cDBC537Ea5d15';
+
+// https://testnet.snowtrace.io/address/0xACd2b1bA0B85FaF0f45D4974Ba8ee538E157fBc6
+export const EXECUTOR_LVL_1 = '0xACd2b1bA0B85FaF0f45D4974Ba8ee538E157fBc6';
+
+// IVotingStrategy https://testnet.snowtrace.io/address/0x8c55a1bf3DC7076522C5cd025f497A498eF90143
+export const VOTING_STRATEGY = '0x8c55a1bf3DC7076522C5cd025f497A498eF90143';
+
+// IDataWarehouse https://testnet.snowtrace.io/address/0xDB23939d4C483E748c2487916fB20d7fF7e39588
+export const DATA_WAREHOUSE = '0xDB23939d4C483E748c2487916fB20d7fF7e39588';
+
+export const CHAIN_ID = 43113;
--- src/ts/GovernanceV3Optimism.ts
+++ src/ts/GovernanceV3Optimism.ts
@@ -2,7 +2,7 @@
 export const CROSS_CHAIN_CONTROLLER = '0x48A9FE90bce5EEd790f3F4Ce192d1C0B351fd4Ca';
 
 // IPayloadsControllerCore https://optimistic.etherscan.io/address/0x0E1a3Af1f9cC76A62eD31eDedca291E63632e7c4
- export const PAYLOADS_CONTROLLER = '0x0E1a3Af1f9cC76A62eD31eDedca291E63632e7c4';
+export const PAYLOADS_CONTROLLER = '0x0E1a3Af1f9cC76A62eD31eDedca291E63632e7c4';
 
 // https://optimistic.etherscan.io/address/0xE3B770Dc4ae3f8bECaB3Ed12dE692c741603e16A
 export const PC_DATA_HELPER = '0xE3B770Dc4ae3f8bECaB3Ed12dE692c741603e16A';
