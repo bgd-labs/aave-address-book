73dc70fafd6ae765f8e2f6aa529de827fee00feb
Lukas <lukasstrassel@googlemail.com>
2025-03-10 08:41:40 +0100

    fix: update snaps

--- src/ts/GovernanceV3PolygonZkEvm.ts
+++ src/ts/GovernanceV3PolygonZkEvm.ts
@@ -2,7 +2,7 @@
 export const CROSS_CHAIN_CONTROLLER = '0xed7e0874526B9BB9E36C7e9472ed7ed324CEeE3B';
 
 // IPayloadsControllerCore https://zkevm.polygonscan.com/address/0xa2d22795E0D85D4Cb14Cbb14Df7bb8fec1016615
-export const PAYLOADS_CONTROLLER = '0xa2d22795E0D85D4Cb14Cbb14Df7bb8fec1016615';
+ export const PAYLOADS_CONTROLLER = '0xa2d22795E0D85D4Cb14Cbb14Df7bb8fec1016615';
 
 // https://zkevm.polygonscan.com/address/0xF1c11BE0b4466728DDb7991A0Ac5265646ec9672
 export const PC_DATA_HELPER = '0xF1c11BE0b4466728DDb7991A0Ac5265646ec9672';
