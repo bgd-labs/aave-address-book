dc168f887ebe0dae04c3d54539805b005474fd66
sendra <polsendra@gmail.com>
2026-01-13 16:08:01 +0100

    feat: Add MegaEth network

---
+++ scripts/configs/networks/megaeth.ts
@@ -0,0 +1,10 @@
+import { ChainId } from '@bgd-labs/toolbox';
+import { NetworkAddresses } from '../types';
+
+export const megaethAddresses: NetworkAddresses = {
+  name: 'Megaeth',
+  chainId: ChainId.megaeth,
+  addresses: {
+    TRANSPARENT_PROXY_FACTORY: '0xEB0682d148e874553008730f0686ea89db7DA412',
+  },
+};
