05eaaffe38f2297f5fef51cdee0339c8dc36b7ba
Lukas <lukasstrassel@googlemail.com>
2026-01-14 15:15:43 +0100

    fix: remove basePath

--- ui/next.config.mjs
+++ ui/next.config.mjs
@@ -2,7 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/aave-address-book' : '',
   images: {
     unoptimized: true,
   },
