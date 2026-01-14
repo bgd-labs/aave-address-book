ff78c9063d1cb300557621bb251a6ff4af16ec1f
Lukas <ls@bgdlabs.com>
2026-01-14 14:01:29 +0100

    feat: ui is automatically deployed to github pages (#1196)
    This change was done in order to remove reliance on us (bgd-labs), the repo and package will be moved to the DAO.

--- ui/next.config.mjs
+++ ui/next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/aave-address-book' : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
