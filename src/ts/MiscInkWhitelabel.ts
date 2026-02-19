8ddbfb167160f96a3314e6c36bf20385cd16e9cc
Harsh Pandey <harshsatishpandey@gmail.com>
2026-02-19 15:20:56 +0530

    feat: add risk agent contracts on ink (#1277)
    Co-authored-by: Lukas <ls@bgdlabs.com>

--- src/ts/MiscInkWhitelabel.ts
+++ src/ts/MiscInkWhitelabel.ts
@@ -5,4 +5,13 @@ export const TRANSPARENT_PROXY_FACTORY = '0x5C2e738F6E27bCE0F7558051Bf90605dD617
 // https://explorer.inkonchain.com/address/0x00C2B13eF4F70Bf1827179Fe6d8facF7cFf6AcD2
 export const PROTOCOL_GUARDIAN = '0x00C2B13eF4F70Bf1827179Fe6d8facF7cFf6AcD2';
 
+// https://explorer.inkonchain.com/address/0x17781Ba226b359e5C1E1ee5ac9E28Ec5b84fd039
+export const AGENT_HUB = '0x17781Ba226b359e5C1E1ee5ac9E28Ec5b84fd039';
+
+// https://explorer.inkonchain.com/address/0x951C37a35cA9e61448B90B7AF5E371cFdA70326C
+export const AGENT_HUB_AUTOMATION = '0x951C37a35cA9e61448B90B7AF5E371cFdA70326C';
+
+// https://explorer.inkonchain.com/address/0xd24790E75799968CE3feD6E27285baD0a26e7e36
+export const RANGE_VALIDATION_MODULE = '0xd24790E75799968CE3feD6E27285baD0a26e7e36';
+
 export const CHAIN_ID = 57073;
