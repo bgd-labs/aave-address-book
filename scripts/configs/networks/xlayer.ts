8bdb6d4842e922fd5c8b66a849fd958dd3972e93
Harsh Pandey <harshsatishpandey@gmail.com>
2026-03-06 17:07:52 +0530

    feat: add xLayer v3.6 contracts (#1302)

---
+++ scripts/configs/networks/xlayer.ts
@@ -0,0 +1,11 @@
+import {ChainId} from '@bgd-labs/toolbox';
+import {NetworkAddresses} from '../types';
+
+export const xLayerAddresses: NetworkAddresses = {
+  name: 'XLayer',
+  chainId: ChainId.xLayer,
+  addresses: {
+    TRANSPARENT_PROXY_FACTORY: '0xF6Dac650dA5616Bc3206e969D7868e7c25805171',
+    PROTOCOL_GUARDIAN: '0xD0D1CcB0391aADF1EaD96814ce7ab4008Ebdb336'
+  },
+};
