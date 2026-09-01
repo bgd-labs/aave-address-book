feaeab6dfd9f994ac6bdfa607858906d37cdddce
AlbertoCentonze <11707683+AlbertoCentonze@users.noreply.github.com>
2026-09-01 05:38:11 +0100

    refactor: update ABI generation

--- scripts/generator/abis.ts
+++ scripts/generator/abis.ts
@@ -1,14 +1,9 @@
-import {ABI_INTERFACES, DOWNLOAD_ABI_INTERFACES, resolveAbiInterface} from 'scripts/configs/abis';
+import {ABI_INTERFACES} from 'scripts/configs/abis';
 
 export function generateABIImports() {
   const jsExports: string[] = [];
-  for (const ENTRY of ABI_INTERFACES) {
-    const {name} = resolveAbiInterface(ENTRY);
+  for (const {name} of ABI_INTERFACES) {
     jsExports.push(`export {${name}_ABI} from './${name}';`);
-  }
-  for (const INTERFACE of DOWNLOAD_ABI_INTERFACES) {
-    const varName = `${INTERFACE.name}_ABI`;
-    jsExports.push(`export {${varName}} from './${INTERFACE.name}';`);
   }
   return jsExports;
 }
